fcbb8fa05512d3e8a8939039144f125d614b9c4d
Juan David <juandominguez2017@u.northwestern.edu>
2016-01-12 00:28:04 -0600

    Refactored index2.html to index.html

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-  res.sendFile('index2.html', {root: __dirname});
+  res.sendFile('index.html', {root: __dirname});
 });
 
 app.listen(process.env.PORT || 5000);
