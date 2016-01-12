47ecac58ef54ca21333dbc49970c381419eef759
Juan David <juandominguez2017@u.northwestern.edu>
2016-01-12 00:19:58 -0600

    adds an express server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+  res.sendFile('index2.html', {root: __dirname});
+});
+
+app.listen(process.env.PORT || 5000);
