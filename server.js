b66750cdc8ccd77181533a50cd31a808a1a2da23
Agam Gupta <agamgupta2018@u.northwestern.edu>
2016-01-20 21:17:50 -0600

    Fixed file rename bug

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html', {root: __dirname});
+  res.sendFile('home.html', {root: __dirname});
 });
 
 app.listen(process.env.PORT || 5000);
